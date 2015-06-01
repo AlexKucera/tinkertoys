8abc99e05a39e54ece28360aa249ec18d5a024c0
Alexander Kucera <a.kucera@babylondreams.de>
2015-06-01 15:34:11 +0200

    Updated for new renders
    Just checking in the most current version. The syntax hasn't changed at all.
    
    Signed-off-by: Alexander Kucera <a.kucera@babylondreams.de>

--- bash/nukerender.sh
+++ bash/nukerender.sh
@@ -1,8 +1,31 @@
 #!/usr/bin/env bash
 
+# Rendering on the command line
+################################
+
+# To render frame 5 of a Nuke script:
+# nuke -F 5 -x myscript.nk
+
+# To render frames 30 to 50 of a Nuke script:
+# nuke -F 30-50 -x myscript.nk
+
+# To render two frame ranges, 10-20 and 34-60, of a Nuke script:
+# nuke -F 10-20 -F 34-60 -x myscript.nk
+
+# To render every tenth frame of a 50 frame sequence of a Nuke script:
+# This renders frames 1, 11, 21, 31, 41.
+# nuke -F 1-50x10 -x myscript.nk
+
+# In a script with two write nodes called WriteBlur and WriteInvert this command just renders frames 1 to 20 from the WriteBlur node:
+# nuke -X WriteBlur myscript.nk 1-20
+
+# To display a list of command line flags (switches) available to you, use the following command:
+# nuke -help
+
+NUKE="/Applications/Nuke9.0v4/NukeX9.0v4.app/NukeX9.0v4"
+FLAGS="-x -m 8 --gpu -f"
+COMMAND="$NUKE $FLAGS"
+
 # basic Nuke render command line
 
-/Applications/Nuke7.0v5/NukeX7.0v5.app/NukeX7.0v5 -x -m 8 -f --gpu -F 20-299 /Volumes/ProjectsRaid/WorkingProjects/spacedigital/space-2013_001-castrol_truck_engine/work/nuke/12sec_v003_comp_v03.nk
-/Applications/Nuke7.0v5/NukeX7.0v5.app/NukeX7.0v5 -x -m 8 -f --gpu /Volumes/ProjectsRaid/WorkingProjects/spacedigital/space-2013_001-castrol_truck_engine/work/nuke/Component-Life-V001-MW_comp_v04_wet.nk
-/Applications/Nuke7.0v5/NukeX7.0v5.app/NukeX7.0v5 -x -m 8 -f --gpu /Volumes/ProjectsRaid/WorkingProjects/spacedigital/space-2013_001-castrol_truck_engine/work/nuke/Fuel-Consumption-V001-MW_comp_v03_dry.nk
-/Applications/Nuke7.0v5/NukeX7.0v5.app/NukeX7.0v5 -x -m 8 -f --gpu /Volumes/ProjectsRaid/WorkingProjects/spacedigital/space-2013_001-castrol_truck_engine/work/nuke/Fuel-Consumption-V001-MW_comp_v03_wet.nk
+$COMMAND -X Write3 "/Volumes/ProjectsRaid/WorkingProjects/manicgrin/manicgrin-2014_001_all-in-a-days-work/work/nuke/deadpixels_v01.nk"
