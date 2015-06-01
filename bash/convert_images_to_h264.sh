e0c446280d29ccc285d68245eef5eaad665a54b7
Alexander Kucera <a.kucera@babylondreams.de>
2015-06-01 15:39:56 +0200

    Refactoring of FFmpeg conversion
    Changed around the syntax a bit for higher quality output and more control over the resulting quality
    
    Signed-off-by: Alexander Kucera <a.kucera@babylondreams.de>

--- bash/convert_images_to_h264.sh
+++ bash/convert_images_to_h264.sh
@@ -9,22 +9,57 @@
 #   Usage: convert_images_to_h264.sh <path to file> <optional: output resolution width (1920, 1280, etc.; defaults to 960)>
 # 
 
+echo "
+Usage: convert_images_to_h264.sh <path to file> 
+<optional: output resolution width (1920, 1280, etc.; defaults to 960)> 
+<optional: framerate ; defaults to 25> 
+<optional: quality (20 is great, 30 is lousy); defaults to 15> 
+<optional: maximum bitrate (average) e.g: 1500 or 2000; defaults to 5000>
+
+"
+
 if [[ ! $1 ]]; then
-	echo "Usage: convert_images_to_h264.sh <path to file> <optional: output resolution width (1920, 1280, etc.; defaults to 960)> <optional: framerate ; defaults to 25> <optional: quality (20 is great, 30 is lousy); defaults to 15>"
 	exit
 fi
 
+nuke="/Applications/Nuke9.0v5/NukeX9.0v5.app/NukeX9.0v5"
+
+
 fullpath=$1
+
+if [[ -d "$fullpath" ]]
+   then
+       pushd "$fullpath" >/dev/null
+       pwd
+       popd >/dev/null
+   elif [[ -e $fullpath ]]
+   then
+       pushd "$(dirname "$fullpath")" >/dev/null
+       fullpath="$(pwd)/$(basename "$fullpath")"
+       popd >/dev/null
+   else
+       echo "$fullpath" does not exist! >&2
+       return 127
+   fi
+
 filename="${fullpath##*/}"                      # Strip longest match of */ from start
+#echo $filename
 dir="${fullpath%$filename}"
 folder=`basename $dir`
 folder=/${folder}/
 basedir="${dir%$folder}"
-base="${filename%.[^.]*.[^.]*}"                       # Strip shortest match of . plus at least one non-dot char from end
-counter_ext="${filename:${#base} + 1}"                  # Substring from len of base thru end
-counter="${counter_ext%.[^.]*}"
+base="${filename%[._][0-9]*.*}"                       # Strip shortest match of . or _ plus any number of numbers a dot and any number of characters from end
+#echo $base
+counter_ext="${filename#$base}"                  # Substring from len of base thru end
+counter_ext="${counter_ext#[._]}"
+#echo $counter_ext
+counter="${counter_ext%.[^._]*}"
+#echo $counter
 ext="${filename:${#base} + 1}"                  # Substring from len of base thru end
 ext="${ext:${#counter} + 1}" 
+#echo $ext
+counter_sep="${filename:${#base}:1}"
+#echo $counter_sep
 
 if [[ -z "$base" && -n "$ext" ]]; then          # If we have an extension and no base, it's really the base
     base=".$ext"
@@ -49,18 +84,38 @@ else
 	quality="15"
 fi
 
-vcodec="-vcodec libx264 -pix_fmt yuv420p -vf scale='$res:trunc(ow/a/2)*2' -g 30 -crf $quality -vprofile high -bf 0"
-acodec="-strict experimental -acodec aac -ab 160k -ac 2"
-sequence="${dir}${base}.%0${#counter}d.jpg"
+if [[ $5 ]]; then
+	let double=$5*2
+	rate="-maxrate ${5}k -bufsize ${double}k"
+else
+	rate="-maxrate 5000k -bufsize 10000k"
+fi
+
+
+vcodec="-vcodec libx264 -preset veryslow -pix_fmt yuv420p -vf scale='$res:trunc(ow/a/2)*2' -g $fps -crf $quality $rate -vprofile high -level 4.0 -bf 0"
+acodec="-c:a libfdk_aac -b:a 160k -ac 2"
+sequence="${dir}${base}${counter_sep}%0${#counter}d.jpg"
 #echo $sequence
 output=${basedir}/${base}.mp4
 #echo $output
-cmd="ffmpeg -y -f image2 -start_number $counter -r $fps -i "$sequence" $acodec $vcodec -f mp4 $output"
+
+if [[ $ext != "jpg" ]]; then
+	if [[ $ext != "JPG" ]]; then
+		nukeconvert="$nuke -t /Volumes/ProjectsRaid/x_Pipeline/x_AppPlugins/Nuke/plugins/convertToJPG.py $fullpath"
+		$nukeconvert
+		sequence="${dir}_tmp/${base}.%0${#counter}d.jpg"
+	fi
+fi
+
+#cmd="ffmpeg -y -f image2 -start_number $counter -r $fps -i "$sequence" $acodec $vcodec -f mp4 $output"
+cmd="ffmpeg -y -f image2 -start_number $counter -r $fps -i "$sequence" $vcodec -f mp4 $output"
 # >> /Volumes/ProjectsRaid/imagesequence-to-h264/convert.log
 #echo $cmd
 $cmd
 if [ "$?" == "0" ]; then
 	echo "Encode Successful!"
+	tmpdir=${dir}_tmp
+	rm -R -f $tmpdir
 #	echo $dir
 	# while true
 	# 	do
--- bash/convert_movie_to_h264.sh
+++ bash/convert_movie_to_h264.sh
@@ -9,8 +9,15 @@
 #   Usage: convert_movie_to_h264.sh <path to file> <optional: output resolution (720, 1080, etc.)>
 # 
 
+echo "
+Usage: convert_images_to_h264.sh <path to file> 
+<optional: output resolution width (1920, 1280, etc.; defaults to 960)> 
+<optional: quality (20 is great, 30 is lousy); defaults to 15> 
+<optional: maximum bitrate (average) e.g: 1500 or 2000; defaults to 5000>
+
+"
+
 if [[ ! $1 ]]; then
-	echo "Usage: convert_movie_to_h264.sh <path to file> <optional: output resolution height (720, 1080, etc.); defaults to 720)> <optional: quality (20 is great, 30 is lousy); defaults to 15>"
 	exit
 fi
 
@@ -33,7 +40,7 @@ fi
 if [[ $2 ]]; then
 	res=${2}
 else
-	res="720"
+	res="960"
 fi
 
 if [[ $3 ]]; then
@@ -42,7 +49,16 @@ else
 	quality="15"
 fi
 
+if [[ $4 ]]; then
+	let double=$4*2
+	rate="-maxrate ${4}k -bufsize ${double}k"
+else
+	rate="-maxrate 5000k -bufsize 10000k"
+fi
+
 dest_file_mp4="${dir}${base}_h264.mp4"
-mp4_vcodec="-vcodec libx264 -pix_fmt yuv420p -vf scale='trunc(oh*a/2)*2:$res' -g 30 -crf $quality -vprofile high -bf 0"
-mp4_acodec="-strict experimental -acodec aac -ab 160k -ac 2"
+mp4_vcodec="-vcodec libx264 -preset veryslow -pix_fmt yuv420p -vf scale='$res:trunc(ow/a/2)*2' -g $fps -crf $quality $rate -vprofile high -level 4.0 -bf 0"
+mp4_acodec="-c:a libfdk_aac -b:a 160k -cutoff 20000 -ac 2"
+echo $mp4_vcodec
+echo $mp4_acodec
 ffmpeg -i "$fullpath" $mp4_acodec $mp4_vcodec -f mp4 "$dest_file_mp4"
