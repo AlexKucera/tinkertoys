f547ee64f05e7daaeac9c146eef3954c7428fd3d
Alexander Kucera <a.kucera@babylondreams.de>
2015-06-01 15:31:47 +0200

    Azure Control Scripts
    Allows automatic stopping of idle VMs, killing the whole farm and starting the whole farm.
    
    Signed-off-by: Alexander Kucera <a.kucera@babylondreams.de>

---
+++ bash/azure_release_the_kraken.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+# Script to automatically start the Azure renderfarm VMs one by one.
+
+RUNNING=$(azure vm list | grep 'ReadyRole' | awk '{print $2}')
+
+if [ -n "$RUNNING" ]; then
+	
+	if [ "$RUNNING" == "Render01" ]; then
+		
+		echo "Render01 is already running. Starting all the others."
+		azure vm start RenderClient01
+		azure vm start RenderClient02
+		azure vm start RenderClient03
+		azure vm start RenderClient04
+		
+	else
+		
+		echo "More then just Render01 is running. Please start the missing VMs by hand."
+		echo "The following machines are already online:"
+		echo $RUNNING
+		
+	fi
+	
+else
+	
+	echo "Starting the whole farm."
+	azure vm start Render01
+	azure vm start RenderClient01
+	azure vm start RenderClient02
+	azure vm start RenderClient03
+	azure vm start RenderClient04
+	
+fi
