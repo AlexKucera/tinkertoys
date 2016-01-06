bdab3586d29d2bc56e4ef53d4d6285eed0c3556e
Alexander Kucera <a.kucera@babylondreams.de>
2016-01-06 13:53:48 +0100

    Code Cleanup
    Signed-off-by: Alexander Kucera <a.kucera@babylondreams.de>

--- python/lib/hash_for_file.py
+++ python/lib/hash_for_file.py
@@ -3,12 +3,13 @@
 
 import os, hashlib
 
+
 def hash_for_file(fileName, block_size=8192):
-	hashvalue = hashlib.sha1()
-	f = open(fileName, "r")
-	while True:
-		data = f.read(block_size)
-		if not data:
-			break
-		hashvalue.update(data)
-	return hashvalue.digest()
+    hashvalue = hashlib.sha1()
+    f = open(fileName, "r")
+    while True:
+        data = f.read(block_size)
+        if not data:
+            break
+        hashvalue.update(data)
+    return hashvalue.digest()
