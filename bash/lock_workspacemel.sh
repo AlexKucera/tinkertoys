72f04733177075206e88723677cbc2f7b2c6c8ec
Alexander Kucera <a.kucera@babylondreams.de>
2025-07-05 12:11:34 +0200

    Cleaned out old unnecessary files

--- bash/lock_workspacemel.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-find /Volumes/Happy-SwapRAID/WorkingProjects -name "workspace.mel" -d -exec setfile -a L "{}" \; -print
--- bash/mayarender.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-# basic Maya render command line
-
-$MAYAPATH/Contents/bin/Render -r mr -proj "/Volumes/ProjectsRaid/WorkingProjects/spacedigital/space-2013_001-castrol_truck_engine" -art -mem 5500 -at -rl masterLayer -v 5 -s 50 -e 500 "/Volumes/ProjectsRaid/WorkingProjects/spacedigital/space-2013_001-castrol_truck_engine/work/maya/scenes/05_render/trialrun_01.mb"
